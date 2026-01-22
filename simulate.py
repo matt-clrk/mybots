ab3559c90bec8f9183e034a20abcc194783c2f50
mclark41 <mclark41@uvm.edu>
2026-01-21 21:08:45 -0500

    final changes

--- simulate.py
+++ simulate.py
@@ -1,4 +1,12 @@
 import pybullet as p
+import time as t
 
 physicsClient = p.connect(p.GUI)
+
+i = 0
+for i in range(0, 1000):
+    p.stepSimulation()
+    t.sleep(1/10)
+    print(i)
+
 p.disconnect()
