e6a36765ed717279cf90dbde23ff467e7f784d6c
mclark41 <mclark41@uvm.edu>
2026-01-21 21:59:21 -0500

    manylinks work

--- simulate.py
+++ simulate.py
@@ -1,14 +1,24 @@
 import pybullet as p
+import pybullet_data
 import time as t
 
+# Create the world
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.loadSDF("box.sdf")
+# Add gravity to the simulated world
+p.setGravity(0, 0, -9.8)
+
+# Add a floor to the world
+planeId = p.loadURDF("plane.urdf")
+
+# Add the cube object
+p.loadSDF("boxes.sdf")
 
 i = 0
 for i in range(0, 1000):
     p.stepSimulation()
-    t.sleep(1/10)
+    t.sleep(1/60)
     print(i)
 
 p.disconnect()
